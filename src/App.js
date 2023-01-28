b8e0e8f09c18642fc241b16a2150b08593c17509
Rob Ferguson <robert.j.ferguson@nasa.gov>
2023-01-28 12:25:45 -0600

    add reactstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import placeholderImg from "./images/undraw_relaxing_walk.svg";
+import { Navbar } from "reactstrap";
 
 function App() {
   const [breeds, setBreeds] = useState([]);
@@ -40,70 +41,7 @@ function App() {
 
   return (
     <div className="d-flex justify-content-center flex-column text-center">
-      <header>
-        <h1 className="mt-4 mb-5">Doggy Directory 🐶</h1>
-      </header>
-      <main role="main">
-        <div className="d-flex justify-content-center">
-          <select
-            className="form-select w-25"
-            aria-label="Select a breed of dog to display results"
-            value={selectedBreed}
-            onChange={(event) => setSelectedBreed(event.target.value)}
-          >
-            <option value="" disabled>
-              Select a breed
-            </option>
-            {breeds.map((breed) => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-          <button
-            type="button"
-            className="btn btn-info mx-2"
-            disabled={!selectedBreed}
-            onClick={searchByBreed}
-          >
-            Search
-          </button>
-        </div>
-        {dogImages.length > 0 && !isLoading && (
-          <div className="px-5 mx-5 text-end" data-testid="results-count">
-            <p className="fs-5">{dogImages.length} results</p>
-          </div>
-        )}
-        <div className="mt-5 d-flex justify-content-center flex-wrap px-5 mx-5">
-          {dogImages.length === 0 && !isLoading && (
-            <img
-              src={placeholderImg}
-              className="mx-auto d-block mt-4 w-50"
-              alt=""
-            />
-          )}
-          {isLoading && (
-            <div className="d-flex align-items-center ">
-              <p className="h1 me-2">Loading</p>
-              <div
-                className="spinner-border ms-auto text-info fs-3"
-                role="status"
-                aria-hidden="true"
-              ></div>
-            </div>
-          )}
-          {dogImages.length > 0 &&
-            !isLoading &&
-            dogImages.map((imgSrc, index) => (
-              <img
-                key={`${index}-${selectedBreed}`}
-                src={imgSrc}
-                className="img-thumbnail w-25"
-                alt={`${selectedBreed} ${index + 1} of ${dogImages.length}`}
-              />
-            ))}
-        </div>
-      </main>
+      <Navbar></Navbar>
     </div>
   );
 }
